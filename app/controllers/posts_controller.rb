5975375f10906504e1935146aef493e393b2c935
Hugo López Mandujano <hugol.soul@gmail.com>
2016-08-12 20:30:44 -0700

    Post Controller & model and the ability to create new post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_post
+
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
